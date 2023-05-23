642cb79e787cb8c632499df0b0ce020197dbf1ba
Malati <preobrazhenskajamv@gmail.com>
2023-05-23 18:02:49 +0300

    Practice num 1

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Radhe Radhe!");
+const numberOfFilms = +prompt("How many films did you see?", '');
+
+console.log(numberOfFilms);
+
+var personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('What film did you see?',''),
+      b = +prompt('What number will you give to it?',''),
+      c = prompt('What film did you see?',''),
+      d = +prompt('What number will you give to it?','');
+
+personalMovieDB.movies [a] = b;
+personalMovieDB.movies [c] = d;
+
+console.log(personalMovieDB);
